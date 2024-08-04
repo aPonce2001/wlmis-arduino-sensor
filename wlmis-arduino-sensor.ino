94fc38268183479d68aa1b517873299dfdc30652
Andrés Ponce <andres.ponce@epn.edu.ec>
2024-08-04 07:10:05 -0500

    test: rgb led tests

--- wlmis-arduino-sensor.ino
+++ wlmis-arduino-sensor.ino
@@ -1,7 +1,27 @@
+#include <RGBLed.h>
+
+#define RED_PIN A0
+#define GREEN_PIN A1
+#define BLUE_PIN A2
+
+RGBLed ledRGB(RED_PIN, GREEN_PIN, BLUE_PIN, RGBLed::COMMON_ANODE);
+
 void setup()
 {
 }
 
 void loop()
 {
+    ledRGB.setColor(RGBLed::RED);
+    delay(500);
+    ledRGB.setColor(RGBLed::YELLOW);
+    delay(500);
+    ledRGB.setColor(RGBLed::GREEN);
+    delay(500);
+    ledRGB.setColor(0, 0, 0);
+    delay(500);
+    ledRGB.setColor(155, 200, 142);
+    delay(500);
+    ledRGB.setColor(255, 255, 255);
+    delay(500);
 }
